756d7ea6435e6597acc09256c47f22331e735ea7
방소영 <bjo8244@naver.com>
2024-05-28 17:52:45 +0900

    [ADD] 인사말 응답 API 구현
    1. JdbcTemplate과 h2 데이터베이스 연결

--- pre-project/src/main/java/hello/DemoApplication.java
+++ pre-project/src/main/java/hello/DemoApplication.java
@@ -1,11 +1,49 @@
 package hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.boot.CommandLineRunner;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class,args);
     }
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    // CommandLineRunner : 어플리케이션 실행 후 특정 코드 실행해주는 인터페이스
+    @Override
+    public void run(String... Strings) throws Exception{
+        jdbcTemplate.execute("DROP TABLE customers IF EXISTS");
+        jdbcTemplate.execute("CREATE TABLE customers(id INT AUTO_INCREMENT, first_name VARCHAR(255), last_name VARCHAR(255))");
+
+
+        // 데이터 삽입
+        List<Object[]> splitUpNames = Arrays.asList("John Woo", "Jeff Dean", "Josh Bloch", "Josh Long").stream()
+                .map(name -> name.split(" "))
+                .collect(Collectors.toList());
+
+        jdbcTemplate.batchUpdate("INSERT INTO customers(first_name, last_name) VALUES (?,?)", splitUpNames);
+
+
+        // 데이터 조회
+        List<Customer> customers = jdbcTemplate.query(
+                "SELECT id, first_name, last_name FROM customers WHERE first_name=?", new Object[]{"josh"},
+                (rs, rowNum) -> new Customer(
+                        rs.getLong("id"),
+                        rs.getString("first_name"),
+                        rs.getString("last_name")
+                )
+        );
+
+    }
+
 }
